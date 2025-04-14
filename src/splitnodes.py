72649c95b523bdc7fd50fb5d5c7fd794ffbd8602
ArtemisNyx3 <artemisnyx3@yahoo.com>
2025-04-14 01:40:50 -0400

    split image and link nodes

--- src/splitnodes.py
+++ src/splitnodes.py
@@ -1,4 +1,5 @@
 from textnode import TextType,TextNode
+from extractmarkdown import extract_markdown_images, extract_markdown_links
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
@@ -29,3 +30,34 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         if (start != len(node.text) -1):
             new_nodes.append(TextNode(node.text[start:],node.text_type))
     return new_nodes
+
+# TODO: Logic to handle duplicate image markdown
+def helper(old_nodes,split_type):
+    new_nodes = []
+    for node in old_nodes:
+        link_mds = extract_markdown_images(node.text) if split_type == TextType.IMAGE else extract_markdown_links(node.text)
+        link_mds = list(link_mds)[::-1]
+        markdown = [node.text]
+        # print(f"Markdown extracted image stack --- {link_mds}")
+        # print(f"Markdown Text Stack            --- {markdown}")
+        while link_mds!= None and len(link_mds) > 0:
+            img_info = link_mds.pop()
+            text = markdown.pop()
+            if text == '':
+                pass
+            sections = text.split(f"![{img_info[0]}]({img_info[1]})", 1)
+            new_nodes.append(TextNode(sections[0],node.text_type)) # Left is the text before the img/link
+            # The link/image
+            new_nodes.append(TextNode(img_info[0],TextType.IMAGE,img_info[1])) if split_type == TextType.IMAGE else  new_nodes.append(TextNode(img_info[0],TextType.LINK,img_info[1])) 
+            markdown.append(sections[1]) # The string that left contains rest of the info
+    # print(f"New Nodes are - {new_nodes}")
+    return new_nodes
+
+def split_nodes_image(old_nodes):
+       return helper(old_nodes, TextType.IMAGE)
+
+    
+
+def split_nodes_link(old_nodes):
+       return helper(old_nodes, TextType.LINK)
+
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -58,17 +58,17 @@ class TestTextNode(unittest.TestCase):
             TextType.TEXT,
         )
         new_nodes = split_nodes_image([node])
-        # self.assertListEqual(
-        #     [
-        #         TextNode("This is text with an ", TextType.TEXT),
-        #         TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
-        #         TextNode(" and another ", TextType.TEXT),
-        #         TextNode(
-        #             "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
-        #         ),
-        #     ],
-        #     new_nodes,
-        # )
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
 
 if __name__ == "__main__":
     unittest.main()
