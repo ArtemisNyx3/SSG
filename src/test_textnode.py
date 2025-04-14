cbaf893467cc32541e60aad69e66c7597b061368
ArtemisNyx3 <artemisnyx3@yahoo.com>
2025-04-14 02:36:36 -0400

    Text to TextNodes done

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,7 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType, text_node_to_html_node
-from splitnodes import split_nodes_delimiter, split_nodes_image
+from splitnodes import split_nodes_delimiter, split_nodes_image, text_to_textnodes
 from extractmarkdown import *
 
 class TestTextNode(unittest.TestCase):
@@ -86,6 +86,25 @@ class TestTextNode(unittest.TestCase):
             ],
             new_nodes,
         )
+    
+    def test_text_to_textnodes(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        text_nodes = text_to_textnodes(text)
+        # self.assertListEqual(
+        #    [
+        #         TextNode("This is ", TextType.TEXT),
+        #         TextNode("text", TextType.BOLD),
+        #         TextNode(" with an ", TextType.TEXT),
+        #         TextNode("italic", TextType.ITALIC),
+        #         TextNode(" word and a ", TextType.TEXT),
+        #         TextNode("code block", TextType.CODE),
+        #         TextNode(" and an ", TextType.TEXT),
+        #         TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+        #         TextNode(" and a ", TextType.TEXT),
+        #         TextNode("link", TextType.LINK, "https://boot.dev"),
+        #     ],
+        #     text_nodes,
+        # )
 
 if __name__ == "__main__":
     unittest.main()
